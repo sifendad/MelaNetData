4ace9ccf78190d91dad936e9be8f7e0b61f6937d
Tilaye Alemu <tilaye@gmail.com>
2019-07-11 18:55:18 +0100

    updated csv generator path

--- src/csv_generator.py
+++ src/csv_generator.py
@@ -8,8 +8,15 @@ def generate_csv(json_file, output_file):
     with open(json_file) as f:
         for line_num, json_line in enumerate(f):
             elements = json.loads(json_line)
-            csv_line = elements["key"] + "," + str(elements["duration"]) + "," + elements["text"]
+            csv_line = "../MelaNet/" + elements["key"] + "," + str(elements["duration"]) + "," + elements["text"]
             data.append(csv_line)
     with open(output_file, 'w') as f:
+        f.write("%s\n" % "wav_filename,wav_filesize,transcript")
         for line in data:
             f.write("%s\n" % line)
+            
+def generate_alphabet(alphabet, output_file):
+    with open(output_file, 'w') as f:
+        for x in alphabet:
+            f.write("%s\n" % x)
+        f.write("%s\n" % " ")
